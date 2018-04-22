e4461a040e1c09137b935c96d3e600b8708a12d3
Jared Hanson <jaredhanson@gmail.com>
2018-04-22 11:54:03 -0700

    Send requests.

--- app/main.js
+++ app/main.js
@@ -1,5 +1,23 @@
 exports = module.exports = function(service) {
-  var api = service;
+  var klamm = require('klamm');
+  
+  
+  var api = {};
+  
+  api.request = function(options, cb) {
+    // TODO: better gatewaying system.  this one is abstract
+    var req = new klamm.Request(options, function() {
+      var res = new klamm.Response();
+      cb(res);
+      
+      // TODO: Out handler to fail the request with error.
+      process.nextTick(function() {
+        service(req, res);
+      })
+    });
+    
+    return req;
+  }
   
   return api;
 };
